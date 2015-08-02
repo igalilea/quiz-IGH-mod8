ce578249d77846f7ff69e1c15b7bfee019838c57
becket <becket73@gmail.com>
2015-08-02 19:43:18 +0200

    Implementación de validación

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: [] });
 });
 
 // Autoload de comandos con :quizId
@@ -22,7 +22,7 @@ router.post('/quizes/create', quizController.create);
 
 /* GET author page */
 router.get('/author', function(req, res){
-	res.render('author', { title: 'Autor'});
+	res.render('author', { title: 'Autor', errors: []});
 });
 
 module.exports = router;
